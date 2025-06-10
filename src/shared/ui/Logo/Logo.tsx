628e15dd05b4baa80a987244af1c1def616844a6
lyapkin <lyapkin992@gmail.com>
2025-06-10 16:27:38 +0500

    logo

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -10,14 +10,43 @@ const Logo = () => {
         height: "100%",
         position: "relative",
         display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "8px",
       }}
     >
-      <Image
-        src={"/images/logo.png"}
-        alt="Логотип компании Строительные технологии"
-        fill
-        style={{ objectFit: "contain" }}
-      />
+      <span
+        style={{
+          aspectRatio: "1/1",
+          position: "relative",
+          height: "100%",
+          display: "flex",
+          minWidth: "0",
+        }}
+      >
+        <Image
+          src={"/images/logo-pic.svg"}
+          alt="Логотип компании Строительные технологии"
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </span>
+      <span
+        style={{
+          height: "56%",
+          aspectRatio: "163/56",
+          position: "relative",
+          display: "flex",
+          minWidth: "0",
+        }}
+      >
+        <Image
+          src={"/images/logo-text.svg"}
+          alt="Логотип компании Строительные технологии"
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </span>
     </Link>
   );
 };
