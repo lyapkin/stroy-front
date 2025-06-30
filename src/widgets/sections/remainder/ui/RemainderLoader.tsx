f488a94ea43ac64cf5386696d18a34d5f3558ea3
lyapkin <lyapkin992@gmail.com>
2025-06-30 13:35:49 +0500

    edited to link

--- src/widgets/sections/remainder/ui/RemainderLoader.tsx
+++ src/widgets/sections/remainder/ui/RemainderLoader.tsx
@@ -9,23 +9,24 @@ const RemainderContent = async () => {
 
   const slides = stockRemainder.map((item) => {
     return (
-      <div key={item.id} className={s.remainderSlide}>
-        <Link
-          href={`/product/${item.slug}/`}
-          className={s.remainderSlide__text}
-        >
+      <Link
+        key={item.id}
+        href={`/product/${item.slug}/`}
+        className={s.remainderSlide}
+      >
+        <div className={s.remainderSlide__text}>
           <p className={s.remainderSlide__title}>{item.name}</p>
           <p className={s.remainderSlide__rest}>
             {item.remainder} {"шт"}
           </p>
-        </Link>
+        </div>
         <Image
           src={item.image || "/images/noimage.jpg"}
           fill
           alt={item.name}
           style={{ objectFit: "cover", zIndex: -1 }}
         />
-      </div>
+      </Link>
     );
   });
 
