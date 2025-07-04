01d57a713b23fc79ab3ab01e6c29cd61b3646f4e
lyapkin <lyapkin992@gmail.com>
2025-07-04 19:07:35 +0500

    added redirect categories

--- src/pagesFSD/catalog/api.ts
+++ src/pagesFSD/catalog/api.ts
@@ -1,10 +1,16 @@
 import { backFetch } from "@/src/shared/api";
 import { Category } from "./types";
+import { permanentRedirect } from "next/navigation";
 
 export const getCategoryApi = async (
   type: "groups" | "categories",
   slug: string
 ): Promise<Category> => {
-  const res = await backFetch(`catalog/${type}/${slug}/`);
+  const res = await backFetch(`catalog/${type}/${slug}/`, {
+    redirect: "manual",
+  });
+  if (res.status === 301) {
+    permanentRedirect(`/catalog${res.headers.get("Location")}`);
+  }
   return res.json();
 };
