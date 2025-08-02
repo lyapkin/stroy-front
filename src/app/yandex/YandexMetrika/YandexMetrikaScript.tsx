f5e770d4467529710dfbb24be512e1fa62c0dbfd
lyapkin <lyapkin992@gmail.com>
2025-08-03 04:30:25 +0500

    .

--- src/app/yandex/YandexMetrika/YandexMetrikaScript.tsx
+++ src/app/yandex/YandexMetrika/YandexMetrikaScript.tsx
@@ -27,15 +27,6 @@ const YandexMetrikaScript = () => {
               accurateTrackBounce:true,
               webvisor:true
         });
-        ym(103542108, 'init', {
-              defer: true,
-              ssr:true,
-              webvisor:true,
-              clickmap:true,
-              ecommerce:"dataLayer",
-              accurateTrackBounce:true,
-              trackLinks:true
-        });
         `}
       </Script>
       <noscript>
@@ -47,6 +38,29 @@ const YandexMetrikaScript = () => {
           />
         </div>
       </noscript>
+      <Script
+        type="text/javascript"
+        id="metrika-counter2"
+        strategy="afterInteractive"
+      >
+        {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103542108', 'ym');
+
+        ym(103542108, 'init', {
+              defer: true,
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+        });
+        `}
+      </Script>
       <noscript>
         <div>
           <img
